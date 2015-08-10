e25ce7356263bcd69fe82827a5c23ed8d90ec474
gedi <gediminas.morkevicius@gmail.com>
2015-08-10 15:47:19 +0300

    move mailer services

--- src/AppBundle/DependencyInjection/AppExtension.php
+++ src/AppBundle/DependencyInjection/AppExtension.php
@@ -16,7 +16,7 @@ class AppExtension extends Extension
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('security.yml');
-        $loader->load('menu.yml');
+        $loader->load('mailer.yml');
 
         if ($container->getParameter('kernel.environment') === 'prod') {
             $loader->load('cache_prod.yml');
