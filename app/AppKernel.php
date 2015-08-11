bf225f38683936e0d3969fb202783c777d0b5fcc
gedi <gediminas.morkevicius@gmail.com>
2015-08-11 17:29:50 +0300

    add audit bundle and merge migrations

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new DataDog\PagerBundle\DataDogPagerBundle(),
+            new DataDog\AuditBundle\DataDogAuditBundle(),
             new EWZ\Bundle\RecaptchaBundle\EWZRecaptchaBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new AppBundle\AppBundle(),
