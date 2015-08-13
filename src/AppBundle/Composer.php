f240ffd2c9c7d418eb5bbdd2446905534df90c0c
gedi <gediminas.morkevicius@gmail.com>
2015-08-13 22:04:18 +0300

    reorganize some stuff

--- src/AppBundle/Composer.php
+++ src/AppBundle/Composer.php
@@ -6,7 +6,13 @@ use Composer\Script\CommandEvent;
 
 class Composer
 {
-    public static function installBinaries(CommandEvent $event)
+    public static function misc(CommandEvent $event)
+    {
+        self::spacingParametersYml($event);
+        self::installBinaries($event);
+    }
+
+    private static function installBinaries(CommandEvent $event)
     {
         if (!is_dir('bin')) {
             $event->getIO()->write(sprintf('The "bin" directory was not found in %s.', getcwd()));
@@ -22,6 +28,21 @@ class Composer
                 continue;
             }
             $event->getIO()->write(sprintf('Installed binary %s.', $dst));
+        }
+    }
+
+    private static function spacingParametersYml(CommandEvent $event)
+    {
+        if (!file_exists($file = 'vendor/incenteev/composer-parameter-handler/Processor.php')) {
+            return;
+        }
+
+        $content = file_get_contents($file);
+        $matches = 0;
+        $content = str_replace('Yaml::dump($actualValues, 99)', 'Yaml::dump($actualValues, 99, 2)', $content, $matches);
+        if ($matches) {
+            file_put_contents($file, $content, LOCK_EX);
+            $event->getIO()->write('Updated spacing for incenteev parameters');
         }
     }
 }
