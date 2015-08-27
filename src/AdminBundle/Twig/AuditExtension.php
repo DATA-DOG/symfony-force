84e4dfbcbc5a4d887e5d73bae317118f5d554d6f
gedi <gediminas.morkevicius@gmail.com>
2015-08-27 14:36:34 +0300

    revisit audit log, allow navigation and lookup in history

--- src/AdminBundle/Twig/AuditExtension.php
+++ src/AdminBundle/Twig/AuditExtension.php
@@ -3,6 +3,7 @@
 namespace AdminBundle\Twig;
 
 use DataDog\AuditBundle\Entity\AuditLog;
+use DataDog\AuditBundle\Entity\Association;
 
 class AuditExtension extends \Twig_Extension
 {
@@ -19,22 +20,42 @@ class AuditExtension extends \Twig_Extension
         return [
             new \Twig_SimpleFunction('audit', [$this, 'audit'], $defaults),
             new \Twig_SimpleFunction('audit_value', [$this, 'value'], $defaults),
+            new \Twig_SimpleFunction('audit_assoc', [$this, 'assoc'], $defaults),
+            new \Twig_SimpleFunction('audit_blame', [$this, 'blame'], $defaults),
         ];
     }
 
     public function audit(\Twig_Environment $twig, AuditLog $log)
     {
-        return $twig->render("AdminBundle::Audit/{$log->getAction()}.html.twig", compact('log'));
+        return $twig->render("AdminBundle:Audit:{$log->getAction()}.html.twig", compact('log'));
+    }
+
+    public function assoc(\Twig_Environment $twig, $assoc)
+    {
+        return $twig->render("AdminBundle:Audit:assoc.html.twig", compact('assoc'));
+    }
+
+    public function blame(\Twig_Environment $twig, $blame)
+    {
+        return $twig->render("AdminBundle:Audit:blame.html.twig", compact('blame'));
     }
 
     public function value(\Twig_Environment $twig, $val)
     {
-        if (is_bool($val)) {
+        switch (true) {
+        case is_bool($val):
             return $val ? 'true' : 'false';
-        } elseif (is_array($val)) {
-            return $twig->render("AdminBundle::Audit/association.html.twig", compact('val'));
+        case is_array($val) && isset($val['fk']):
+            return $this->assoc($twig, $val);
+        case is_array($val):
+            return json_encode($val);
+        case is_string($val):
+            return strlen($val) > 60 ? substr($val, 0, 60) . '...' : $val;
+        case is_null($val):
+            return 'NULL';
+        default:
+            return $val;
         }
-        return $val;
     }
 
     public function getName()
